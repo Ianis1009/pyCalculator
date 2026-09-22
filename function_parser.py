86f2b9a0db4c49a4115489e9568e7757b14bfd4e
Iosif Ianis-Cosmin <iosifianis1@gmail.com>
2026-09-22 20:53:43 +0300

    add functions in parser.py

--- function_parser.py
+++ function_parser.py
@@ -1,1 +1,16 @@
-#TODO
+import sympy as sp
+
+x = sp.symbols("x")
+
+def parse_function (expression):
+    pass # function -> math expression SymPy
+
+def validate_function (function):
+    pass # verify if is instance of SymPy
+
+def create_numeric_function (function):
+    pass # transform in numerical function
+
+def get_function (expression):
+    pass # return the math function
+
