0e5a8d3f7affb36ff21f8c359b582d7eb8c5303a
Iosif Ianis-Cosmin <iosifianis1@gmail.com>
2026-09-22 21:05:27 +0300

    add functions in integrator.py

--- integrator.py
+++ integrator.py
@@ -1,1 +1,19 @@
-#TODO
+import numpy as np 
+import sympy as sp 
+
+from function_parser import x
+
+def analytical_integral(function, a, b):
+    pass 
+
+def trapezoidal_integral(numeric_function, a, b, n=1005):
+    pass 
+
+def simpson_integral(numeric_function, a, b, n = 1005):
+    pass
+
+def calculate_error (exact_value, approximate_value):
+    pass
+
+def calculate_integrals (function, numeric_function, a, b, n =1005):
+    pass 
