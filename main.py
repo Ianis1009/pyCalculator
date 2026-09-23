0c584134091d2d30942e13c0d62cccc862604c9f
Iosif Ianis-Cosmin <iosifianis1@gmail.com>
2026-09-23 09:15:02 +0300

    add get_float_input

--- main.py
+++ main.py
@@ -3,4 +3,11 @@ from integrator import calculate_integrals
 from plotter import create_dashboard
 
 def get_float_input (message):
-    pass
+    while True:
+        try:
+            return float(input(message))
+        except ValueError:
+            print("Valoare invalida -> ", "Introdu un numar.")
+
+def get_function_input():
+    
