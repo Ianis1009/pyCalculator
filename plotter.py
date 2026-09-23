6ce22521c7e3dfd9ca72ddfbbc267c921e004f03
Iosif Ianis-Cosmin <iosifianis1@gmail.com>
2026-09-23 08:55:10 +0300

    add get_save_values

--- plotter.py
+++ plotter.py
@@ -14,3 +14,11 @@ OUTPUT_DIR = "output"
 def prepare_output_directory():
     os.makedirs(OUTPUT_DIR, exist_ok=True)
 
+def get_safe_values (numeric_function, points):
+    while np.errstate(divide="ignore", invalid="ignore", over="ignore"):
+        values = numeric_function(points)
+    values =  np.asarray(values, dtype=float)
+    values[~np.isfinite(values)] = np.nan
+
+    return values
+
