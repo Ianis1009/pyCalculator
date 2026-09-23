4f43787e93fc40ddf55b969eda980f363cf66f7c
Iosif Ianis-Cosmin <iosifianis1@gmail.com>
2026-09-23 08:49:53 +0300

    update plotter

--- plotter.py
+++ plotter.py
@@ -1,1 +1,13 @@
-#TODO
+import os 
+import matplotlib
+
+matplotlib.use("Agg") # WSL2
+
+import matplotlib.pyplot as plt
+import numpy as np
+import sympy as sp
+
+from function_parser import x
+
+OUTPUT_DIR = "output"
+
