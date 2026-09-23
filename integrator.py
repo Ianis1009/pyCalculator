d19509c9bfe0ad93ab74c51e3902a225753a5fdb
Iosif Ianis-Cosmin <iosifianis1@gmail.com>
2026-09-23 09:31:46 +0300

    modify n value

--- integrator.py
+++ integrator.py
@@ -6,14 +6,14 @@ from function_parser import x
 def analytical_integral(function, a, b):
     return sp.integrate(function, (x, a, b))
 
-def trapezoidal_integral(numeric_function, a, b, n=1005):
+def trapezoidal_integral(numeric_function, a, b, n=1006):
     points = np.linspace(a, b, n + 1)
     values = numeric_function(points)
     h = (b - a) / n 
     result = h * (0.5 * values[0] + np.sum(values[1:-1]) + 0.5 * values[-1])
     return float(result)
 
-def simpson_integral(numeric_function, a, b, n = 1005):
+def simpson_integral(numeric_function, a, b, n = 1006):
     if n % 2 != 0:
         raise ValueError("n trebuie sa fie par pentru metoda Simpson.")
     points = np.linspace(a, b, n + 1)
@@ -27,7 +27,7 @@ def calculate_error (exact_value, approximate_value):
     exact = float(sp.N(exact_value))
     return abs(exact_value - approximate_value)
 
-def calculate_integrals (function, numeric_function, a, b, n =1005):
+def calculate_integrals (function, numeric_function, a, b, n =1006):
     exact = analytical_integral(function, a, b)
     trapezoidal = trapezoidal_integral(numeric_function, a, b, n)
     simpson = simpson_integral(numeric_function, a, b, n)
